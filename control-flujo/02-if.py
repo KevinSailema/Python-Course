3c8663ac37ab270a03420390d5202db542db5b35
Kevin Sailema <kevin.sailema@epn.edu.ec>
2024-08-03 22:00:00 -0500

    Agregando .gitignore

---
+++ control-flujo/02-if.py
@@ -0,0 +1,11 @@
+edad = 60
+if edad > 17:
+    print("Puede ver la película con super descuento")  #Importante, poner espacios de identación.
+elif edad > 54:
+    print("Puedes ver la película con descuento")
+elif edad > 53:
+    print("Puedes ver la película")
+else:
+    print("No puedes entrar")
+    print("Ve a otro lado")
+print("Listo")  
